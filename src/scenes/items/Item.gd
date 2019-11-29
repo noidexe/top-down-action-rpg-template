2c70ced6d12466375122cf1a0b4c28522752d105
Lisandro Lorea <lisandrolorea@gmail.com>
2019-11-29 03:03:54 -0300

    Add item scene

---
+++ src/scenes/items/Item.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+export(String) var item_type = "Generic Item"
+export(int) var amount = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("body_entered", self, "_on_Item_body_entered")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Item_body_entered(body):
+	if body is Player:
+		disconnect("body_entered", self, "_on_Item_body_entered")
+		Inventory.add_item(item_type, amount)
+		queue_free()
+	pass # Replace with function body.
